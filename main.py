4189da6e0ffd9003aca19a88a84c05665f5089c5
aaron <aaron>
2026-03-24 16:54:59 -0400

    Added option print code

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ from WordAnalyzer import WordAnalyzer
 
 # this is here because cwd is not consistent and changes depending on where the file is executed
 project_dir = Path(__file__).resolve().parent
+document_path = project_dir / "documents"
+
+documents = {
+    "1": document_path / "file_1.txt",
+    "2": document_path / "file_2.txt",
+    "3": document_path / "file_3.txt",
+    "4": document_path / "file_4.txt"
+}
+
+def display_documents(options: dict[str: Path]):
+    for pos, item in options.items():
+        print(f"{pos}. {item.name()}")
 
 def main():
     pass
