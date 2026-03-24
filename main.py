79a614c93958bd46f9a045c797737fefe83a113b
aaron <aaron>
2026-03-24 16:31:00 -0400

    Added main and gitignore

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Name: Word Counter
+Author: Luke Atkins
+Date: 3/24/2026
+Purpose: Counts number of words within specified documents
+Starter Code: No starter code used.
+"""
+from pathlib import Path
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
